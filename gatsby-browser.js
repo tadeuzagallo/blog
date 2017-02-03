688146ec463c255b6000ce72a4462dff1a99cb3c
Tadeu Zagallo <tadeuzagallo@gmail.com>
2017-02-03 23:43:53 +0100

    Add linkid to google analytics

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,6 +2,7 @@ import ReactGA from 'react-ga'
 import {config} from 'config'
 
 ReactGA.initialize(config.googleAnalyticsId);
+ReactGA.plugin.require('linkid');
 
 exports.onRouteUpdate = (state, page, pages) => {
   ReactGA.pageview(state.pathname);
