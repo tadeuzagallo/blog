4d3512cb2794bb11a5913d7518a736f2e3ffa02e
Tadeu Zagallo <tzagallo@apple.com>
2022-01-03 20:22:29 +0100

    Fresh start

--- gatsby-browser.js
+++
@@ -1,9 +0,0 @@
-import ReactGA from 'react-ga'
-import {config} from 'config'
-
-ReactGA.initialize(config.googleAnalyticsId);
-ReactGA.plugin.require('linkid');
-
-exports.onRouteUpdate = (state, page, pages) => {
-  ReactGA.pageview(state.pathname);
-};
