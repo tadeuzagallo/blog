1da10434ec01f7f3e7ae033b573a5f86564f4d8c
Tadeu Zagallo <tzagallo@apple.com>
2022-01-09 13:33:50 +0100

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+touch .nojekyll
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:tadeuzagallo/blog.git main:gh-pages
+
+cd -
